14922f254cf15bf1e39d928975500c76709e10a5
jason <jason_doan94@yahoo.com>
2020-07-12 21:22:50 -0400

    confirm working

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          testing testing
+          confirmed working
         </a>
       </header>
     </div>
