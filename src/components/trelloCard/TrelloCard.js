15603886104740fba760455942bd4872899fff64
jason <jason_doan94@yahoo.com>
2020-07-12 22:36:02 -0400

    folder setup and files

---
+++ src/components/trelloCard/TrelloCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+
+const TrelloCard = () => {
+  return (
+    <Card>
+      <Typography color="textSecondary" gutterBottom>
+        Word of the Day
+      </Typography>
+    </Card>
+  );
+};
+
+export default TrelloCard;
